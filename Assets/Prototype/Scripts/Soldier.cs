367723aee9d768a6051d77b14577f5cbe564f0da
Rink <rinkwilbrink1@gmail.com>
2019-02-18 12:43:14 +0100

    Start to attacks
    making a script that can call all attacks with an interface, this way there is ONE script that you can put on all types of soldier and still let them have their unique attacks

---
+++ Assets/Prototype/Scripts/Soldier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Factions
+{
+	MagicSwordsman = 1, Magic = 2
+}
+public enum SoldierType
+{
+	looter = 1, Melee = 2,
+	Ranged = 3, Mage = 4
+}
+
+public class Soldier : MonoBehaviour
+{
+	// Variables
+
+	[Header("Select faction and soldier type")]
+	[SerializeField] private Factions factions;
+	[SerializeField] private SoldierType soldierType;
+
+	[Header("Soldier Stats")]
+	[SerializeField] private int Health;
+	[SerializeField] private int damage;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
