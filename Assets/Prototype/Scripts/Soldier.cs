05f771be23766bea79d54ff7a10bec0933b1998f
Rink <rinkwilbrink1@gmail.com>
2019-03-04 12:45:18 +0100

    Soldiers can move now
    They move to the correct direction of where they need to go for their faction

--- Assets/Prototype/Scripts/Soldier.cs
+++ Assets/Prototype/Scripts/Soldier.cs
@@ -2,35 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Factions
-{
-	MagicSwordsman = 1, Magic = 2
-}
 public enum SoldierType
 {
-	looter = 1, Melee = 2,
-	Ranged = 3, Mage = 4
+	looter = 0, Melee = 1,
+	Ranged = 2, Mage = 3
 }
 
 public class Soldier : MonoBehaviour
 {
 	// Variables
-
 	[Header("Select faction and soldier type")]
-	[SerializeField] private Factions factions;
-	[SerializeField] private SoldierType soldierType;
+	[SerializeField] public SoldierType soldierType;
 
 	[Header("Soldier Stats")]
 	[SerializeField] private int Health;
 	[SerializeField] private int damage;
+	[SerializeField] private int speedMultiplier;
+
+	[Header("Debug attributes")]
+	[SerializeField] private bool allowedToMove;
+
+	// Private Variables
+	private Vector3 position;
+	private string attackTag;
 
     void Start()
     {
-        
-    }
+		// Set the vector equal to the position that the soldier is on at the start of its creation
+		position.x = transform.position.x;
+		position.y = transform.position.y;
+		position.z = transform.position.z;
+		// Settings for individual sides
+		if(!allowedToMove)
+		{
+			speedMultiplier = 0;
+		}
+		switch (gameObject.tag)
+		{
+			case "Swordsmen":
+				attackTag = "Mage";
+				break;
+			case "Mage":
+				speedMultiplier = -speedMultiplier;
+				attackTag = "Swordsmen";
+				break;
+		}
+	}
     
     void Update()
     {
-        
+		position.x += speedMultiplier * Time.deltaTime;
+
+		if(Health <= 0)
+		{
+			Die();
+		}
+		//Update the position of the object
+		transform.position = new Vector3(position.x, position.y, position.z);
     }
+
+	private void Die()
+	{
+		Debug.Log(gameObject.name + " has Died");
+	}
 }
---
+++ Assets/Prototype/Scripts/SpawnSoldiers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSoldiers : MonoBehaviour
+{
+	// Variables
+	[SerializeField] GameObject soldier;
+	[SerializeField] private KeyCode kc_SpawnSoldier;
+	
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        if(Input.GetKeyDown(kc_SpawnSoldier))
+		{
+			Debug.Log("Pressed " + kc_SpawnSoldier);
+			Instantiate(soldier, new Vector3(0, .5f, 0), Quaternion.identity);
+		}
+    }
+}
