90e4859077456e01432627ce3b52beb605d4b57b
Ahmad Najafi <ninjahmad69@gmail.com>
2019-04-01 09:18:25 +0200

    CharacterGUI

---
+++ Assets/Scripts/CharacterGUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGUI : MonoBehaviour {
+    private void OnGUI() {
+        Vector3 targetPos;
+        targetPos = Camera.main.WorldToScreenPoint(transform.position);
+        GUI.Box(new Rect(targetPos.x, ( Screen.height - targetPos.y ) - 35, 80, 20), gameObject.GetComponent<Soldier>().soldierType.ToString());
+    }
+}
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,31 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
-    [SerializeField] private GameObject looter;
-    [SerializeField] private GameObject melee;
-    [SerializeField] private GameObject ranged;
-    [SerializeField] private GameObject spellcaster;
+    [SerializeField] private GameObject[] enemiesToSpawn;
     [SerializeField, Range(0, 5)] private float spawnCooldown = 2f;
     private GameController game;
     public int looterUnits;
-    bool unitSpawned;
+    private int unitsSpawned;
+    bool unitSpawned = false;
     float spawnTimer;
 
     private void Start() {
         game = FindObjectOfType<GameController>();
-        Instantiate(looter, transform.position, transform.rotation);
+        Instantiate(enemiesToSpawn[0], transform.position, transform.rotation);
     }
-
+    float testTimer;
     private void Update() {
         if ( game.enemyPoints >= 10 && looterUnits < 2 && !unitSpawned )
-            InstantiateUnit(looter, 10);
-        if ( game.enemyPoints >= 20 && looterUnits < 333 && !unitSpawned )
-            InstantiateUnit(melee, 20);
-        if ( game.enemyPoints >= 30 && looterUnits < 333 && !unitSpawned )
-            InstantiateUnit(ranged, 30);
-        if ( game.enemyPoints >= 30 && looterUnits < 333 && !unitSpawned )
-            InstantiateUnit(spellcaster, 30);
+            InstantiateUnit(enemiesToSpawn[0], 10);
+        testTimer += Time.deltaTime;
+        if ( unitsSpawned <= 100 && testTimer > 4f && game.enemyPoints >= 20 ) {
+            int rand = Random.Range(1, enemiesToSpawn.Length);
+            InstantiateUnit(enemiesToSpawn[rand], 20);
+            testTimer = 0f;
+        }
 
+        //if ( game.enemyPoints >= 20 && looterUnits < 333 && !unitSpawned )
+        //    InstantiateUnit(melee, 20);
+        //if ( game.enemyPoints >= 30 && looterUnits < 333 && !unitSpawned )
+        //    InstantiateUnit(ranged, 30);
+        //if ( game.enemyPoints >= 30 && looterUnits < 333 && !unitSpawned )
+        //    InstantiateUnit(spellcaster, 30);
+        print(looterUnits);
         if ( unitSpawned )
             spawnTimer += Time.deltaTime;
         if ( spawnTimer > spawnCooldown ) {
@@ -39,6 +44,7 @@ public class EnemyBehaviour : MonoBehaviour {
     private void InstantiateUnit( GameObject go, int cost ) {
         Instantiate(go, transform.position, transform.rotation);
         Debug.Log(go.name + " spawned");
+        unitsSpawned++;
         unitSpawned = true;
         game.enemyPoints -= cost;
     }
